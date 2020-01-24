a7e7db471195552931be78e2e7d462a84e1cf890
Jacqueline Freeland <secretartist272@gmail.com>
2020-01-24 21:37:09 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 22, "M", "tan brown", "Choleric", 0);
+INSERT INTO bears VALUES (2,"Rowdy", 18, "M", "black", "Sanguine", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 7, "F", "dark brown", "Gentle", 1);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "Green", "Decisive", 1);
+INSERT INTO bears VALUES (5,"Melissa", 16, "F", "dark brown", "Sanguine", 1);
+INSERT INTO bears VALUES (6,"Grinch", 5, "M", "Black", "Misserable", 1);
+INSERT INTO bears VALUES (7,"Wendy", 2, "F", "Blue", "Naive", 0);
+INSERT INTO bears VALUES (8,null, 20, "F", "black", "Aggressive", 0);
